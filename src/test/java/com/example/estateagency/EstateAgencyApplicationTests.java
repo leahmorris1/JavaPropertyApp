712d323532a80f67a796e72f58e5eecd8f8bed0c
fisik <fisiko2000@gmail.com>
2023-05-10 10:26:03 +0100

    Initial commit

---
+++ src/test/java/com/example/estateagency/EstateAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estateagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
