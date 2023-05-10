9094af049f52b2584f5ae6c6e8397a5c847e316e
Leah M <leahsummer@btinternet.com>
2023-05-10 10:12:52 +0100

    hello

---
+++ src/main/java/com/example/propertyapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.propertyapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PropertyAppApplication.class);
+    }
+
+}
---
+++ src/test/java/com/example/propertyapp/PropertyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
